d62025629f43dd9f543819453a34b720b7f805f9
Bruce Whealton, Jr <bruce@whealton.info>
2012-11-29 06:18:50 -0500

    Add more source code directories

---
+++ facebook/wordsalad/_/js/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://ogp.me/ns#">
+<head>
+<meta charset="utf-8" />
+<title>My Family Lineage App</title>
+<link href='https://fonts.googleapis.com/css?family=Exo:700,700italic|Average' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="_/css/mystyle.css">
+<script src="_/js/jquery-1.8.0.min.js"></script>
+<script type="text/javascript" src="_/js/myscript.js"></script>
+</head>
+<body>
+<div id="fb-root"></div>
+  <div id="main">
+    <div id="fb-root"></div>
+    <div id="mymessage"></div>
+    <div id="welcome"></div>
+    <div id="invite"><h2><a href="https://www.facebook.com/dialog/pagetab?app_id=427064894002819&next=https://my-family-lineage.com/facebook/">Add this app to your Page</a></h2></div>
+    <div id="blog"></div>
+    <div class="fb-comments" data-width="400" data-href="https://apps.facebook.com/my-family-lineage" data-num-posts="2"></div>
+  
+<div id="chat">      <h2>Chat Section</h2>
+      <p>Chat (Live) with others about discovering your roots - your Genealogy</p>
+  <div class="fb-live-stream" data-width="400" data-height="400" data-always-post-to-friends="true"></div></div>
+  </div>
+  <div id="sidebar">
+      <div id="socialplugins">
+          <h2 class="label">Like This App?</h2>
+          <p>Help us share My Family Lineage - Our Genealogy Application by liking this app. Click on the like button below.</p>
+          <div class="fb-like" data-href="http://apps.facebook.com/my-family-lineage" data-width="200"></div>
+           <h2 class="label">Who loves us</h2>
+        <p>Here's a list of some your friends who also like this app.</p>
+        <div class="fb-facepile" data-width="240"></div>  
+    </div><!--social plugins -->
+  <div id="videogroup"></div><!--videogroup -->
+  </div>
+  <!--sidebar --> 
+  <!--content -->
+  <script type="text/javascript" src="http://gdata.youtube.com/feeds/api/videos?q=genealogy%7C%22genealogy+semantic+web%22%7C%22linked+data+genealogy%22&alt=json-in-script&callback=populateVideos&max-results=25"></script>
+  <script type="application/javascript" src="http://blog.my-family-lineage.com/?json=get_recent_posts&count=5&callback=populateStories"></script>
+</body>
+</html> 
